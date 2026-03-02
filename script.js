01dc34dd969dd2bac9665827f449a30750bf6144
youfrig <85086003+youfrig@users.noreply.github.com>
2026-03-02 11:20:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+body {
+    font-family: Arial;
+    background: #f2f2f2;
+}
+
+.container {
+    width: 400px;
+    margin: 100px auto;
+    background: white;
+    padding: 30px;
+    border-radius: 8px;
+    text-align: center;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+}
+
+button {
+    padding: 8px 20px;
+    margin-top: 10px;
+    cursor: pointer;
+}
